a5696cbc463cfedfbd299daeae04643cc1cc4b00
Nikita <k0956493763@gmail.com>
2024-04-01 14:22:14 +0200

    made site without adaptive

--- js/script.js
+++ js/script.js
@@ -1,25 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let currentSlide = 1;
-    let totalSlides = 2;
+  let currentSlide = 1;
+  let totalSlides = 2;
 
-    let prevButton = document.getElementById("prevButton");
-    let nextButton = document.getElementById("nextButton");
+  let prevButton = document.getElementById("prevButton");
+  let nextButton = document.getElementById("nextButton");
 
-    prevButton.addEventListener("click", () => {
-        currentSlide = currentSlide - 1 <= 0 ? totalSlides : currentSlide - 1;
-        showSlide(currentSlide);
-    });
-
-    nextButton.addEventListener("click", () => {
-        currentSlide = currentSlide + 1 > totalSlides ? 1 : currentSlide + 1;
-        showSlide(currentSlide);
-    });
-
-    function showSlide(slideNumber) {
-        const slides = document.getElementsByClassName("slider_content");
-        Array.from(slides).forEach((slide) => slide.classList.add("hidden"));
-        slides[slideNumber - 1].classList.remove("hidden");
-    }
-
+  prevButton.addEventListener("click", () => {
+    currentSlide = currentSlide - 1 <= 0 ? totalSlides : currentSlide - 1;
     showSlide(currentSlide);
+  });
+
+  nextButton.addEventListener("click", () => {
+    currentSlide = currentSlide + 1 > totalSlides ? 1 : currentSlide + 1;
+    showSlide(currentSlide);
+  });
+
+  function showSlide(slideNumber) {
+    const slides = document.getElementsByClassName("slider_content");
+    Array.from(slides).forEach((slide) => slide.classList.add("hidden"));
+    slides[slideNumber - 1].classList.remove("hidden");
+  }
+
+  showSlide(currentSlide);
 }); // фух, ели успел(тут могла быть ваша утка)
+
+// анимация счета
+const counterElement = document.querySelector("#countedNumber");
+let isAnimated = false;
+
+document.addEventListener("scroll", () => {
+  if (
+    counterElement.getBoundingClientRect().top < window.innerHeight * 0.75 &&
+    !isAnimated
+  ) {
+    isAnimated = true;
+    startCount();
+  }
+});
+
+function startCount() {
+  let currentValue = 0;
+  let max = +counterNumber.getAttribute("data-value");
+
+  let pace = 100;
+
+  let interval = setInterval(() => {
+    if (pace >= max * 0.75) {
+      pace = 10;
+    }
+    if (currentValue >= max) {
+      clearInterval(interval);
+    }
+    counterNumber.innerText = currentValue;
+    currentValue++;
+  }, pace);
+}
